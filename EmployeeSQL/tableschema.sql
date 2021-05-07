683669f3a6403691240986372ff37a511d379041
William Serene <willserene@gmail.com>
2021-05-07 11:15:04 -0600

    table schema working and csvs imported

--- EmployeeSQL/tableschema.sql
+++ EmployeeSQL/tableschema.sql
@@ -1,10 +1,10 @@
+DROP TABLE IF EXISTS employees, dept_emp, departments, dept_manager, titles, salaries CASCADE;
+
 CREATE TABLE titles (
     title_id varchar   NOT NULL,
     title varchar   NOT NULL,
     	PRIMARY KEY (title_id)
 );
-
-select * from titles
 
 CREATE TABLE employees (
     emp_no int   NOT NULL,
@@ -26,21 +26,21 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE departments (
-    dept_no int   NOT NULL,
+    dept_no varchar   NOT NULL,
     dept_name varchar   NOT NULL,
     	PRIMARY KEY (dept_no)
 );
 
 CREATE TABLE dept_emp (
     emp_no int   NOT NULL,
-    dept_no int   NOT NULL,
+    dept_no varchar   NOT NULL,
     	PRIMARY KEY (emp_no, dept_no),
 		FOREIGN KEY (emp_no) references employees (emp_no),
 		FOREIGN KEY (dept_no) references departments (dept_no)
 );
 
 CREATE TABLE dept_manager (
-    dept_no int   NOT NULL,
+    dept_no varchar   NOT NULL,
     emp_no int   NOT NULL,
     	PRIMARY KEY (dept_no, emp_no),
 		FOREIGN KEY (dept_no) references departments (dept_no),
